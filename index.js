44b7d34823f2b7583c86c67e0c40454f3df85e4d
Sunny Asar <asarsunny@gmail.com>
2020-10-05 21:16:46 +0100

    added index.js and Dockerfile

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.json([
+    {
+      name: 'sunny',
+      occupation: 'developer'
+    }
+  ])
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
